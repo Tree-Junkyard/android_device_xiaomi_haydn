e62fc60a79db534c3773265dd69823b5ecea9058
Astridxx <muratkozan350@gmail.com>
2023-06-01 16:09:40 +0300

    haydn: crDroid-Ify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,46 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
+# Device common
+echo 'Cloning common device tree'
+git clone https://github.com/Astridxx/android_device_xiaomi_sm8350-common -b crDroid-13 device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone --depth=1 https://github.com/Astridxx/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+cd kernel/xiaomi/sm8350
+git submodule init && git submodule update KernelSU
+cd ../../..
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
+
+# Vendor common
+echo 'Cloning common vendor tree'
+git clone https://github.com/Astridxx/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+
 # Firmware
 echo 'Cloning firmware'
 git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+
+# Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 
 # Camera
 echo 'Cloning Leica Camera'
 git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
 
-echo 'Adding slow-mo fix for Leica Camera'
+echo 'Adding Slow-mo fix for Leica Camera'
 cd hardware/interfaces
 wget https://github.com/Astridxx/android_hardware_interfaces/commit/ca6551863fb82110688452981572d44a26806681.patch
 patch -p1 <ca6551863fb82110688452981572d44a26806681.patch
+cd ../..
+
+echo 'Setting combined qs header in status bar to false'
+cd frameworks/base
+wget https://github.com/garry-rogov/android_frameworks_base/commit/f7935b46c9d5bb8c563574622762557b62ba3bdc.patch
+patch -p1 <f7935b46c9d5bb8c563574622762557b62ba3bdc.patch
 cd ../..
 
 echo 'delete vendorsetup.sh from device tree once this is done'
